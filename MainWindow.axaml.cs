f775c53bd63235fcc07f9fbfdaa1df1d97c2062f
Philip <fsarg2007@gmail.com>
2024-02-14 00:43:29 +0300

    hotfix

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -21,6 +21,17 @@ abstract class Shape
 
     abstract public void Draw(Canvas canv);
 	public virtual bool IsInside(double x, double y) { if (this.x == x && this.y == y) return true; else return false; }
+
+	public double X
+	{
+		get { return x; }
+		set { x = value; }
+	}
+	public double Y
+	{
+		get { return x; }
+		set { x = value; }
+	}
 }
 class Square : Shape
 {
@@ -47,7 +58,7 @@ class Square : Shape
 }
 class Triangle : Shape
 {
-	private double z = Radius / 2 * Math.Pow(3, 0.5);
+	private double z = Radius / 4 * Math.Pow(3, 0.5);
 	public Triangle(double x, double y)
 	{
 		this.x = x;
@@ -56,8 +67,8 @@ class Triangle : Shape
 	}
 	public override void Draw(Canvas canv)
 	{
-
-		List<Avalonia.Point> tmp = new List<Avalonia.Point>() { new Avalonia.Point(this.x - z, this.y + Radius / 2), new Avalonia.Point(this.x + z, this.y + Radius / 2), new Avalonia.Point(this.x, this.y - Radius) };
+		
+		List<Avalonia.Point> tmp = new List<Avalonia.Point>() { new Avalonia.Point(this.x - z, this.y + Radius / 4), new Avalonia.Point(this.x + z, this.y + Radius / 4), new Avalonia.Point(this.x, this.y - Radius/2) };
 		Polygon shape = new Polygon() { Points = tmp, Fill = Avalonia.Media.Brushes.DarkRed, StrokeThickness = 1, Stroke = Avalonia.Media.Brushes.Black };
 		canv.Children.Add(shape);
 		Canvas.SetLeft(shape, 0);
@@ -97,6 +108,7 @@ class Circle : Shape
 public partial class MainWindow : Window
 {
 	List<Shape> shapes = new List<Shape>();
+	private string type = "square";
 	public MainWindow()
 	{
 		InitializeComponent();
@@ -116,27 +128,65 @@ public partial class MainWindow : Window
 		double Y = e.GetCurrentPoint(canv).Position.Y;
 		foreach (Shape shape in shapes)
 		{
-			if (shape.IsInside(X, Y)) return;
+			if (shape.IsInside(X, Y))
+			{
+				return;
+			}
+
 		}
-		shapes.Add(new Triangle(X, Y));
+		if(type == "circle")
+		{
+			shapes.Add(new Circle(X, Y));
+		}
+		else if(type == "square")
+		{
+			shapes.Add(new Square(X, Y));
+		}
+		else if (type == "triangle")
+		{
+			shapes.Add(new Triangle(X, Y));
+		}
+
 		Redraw();
 	}
-	private Button CreateBtn(int w, string content, string name)
+	private void PointerReleased(object sender, PointerReleasedEventArgs e)
+	{
+
+	}
+	private Button CreateBtn(int w, string content, string name, EventHandler<RoutedEventArgs> x)
 	{
 		Button b = new Button();
 		b.Width = w;
 		b.Height = 30;
 		b.Content = content;
 		b.Name = name;
+		b.Click += x;
 		b.Background = Avalonia.Media.Brushes.Gray;
 		b.BorderBrush = Avalonia.Media.Brushes.Black;
 		return b;
 	}
 	private void AddButtons()
 	{
-		Buttons.Children.Add(CreateBtn(65, "square", "switch_to_square"));
-		Buttons.Children.Add(CreateBtn(57, "circle", "switch_to_circle"));
-		Buttons.Children.Add(CreateBtn(72, "triangle", "switch_to_triangle"));
+		Button b_square =CreateBtn(65, "square", "switch_to_square", button_click_square);
+		Button b_circle = CreateBtn(57, "circle", "switch_to_circle", button_click_circle);
+		Button b_triangle = CreateBtn(72, "triangle", "switch_to_triangle", button_click_triangle);
+		Buttons.Children.Add(b_square);
+		Buttons.Children.Add(b_circle);
+		Buttons.Children.Add(b_triangle);
+
+		
 	}
 
+	private void button_click_circle(object sender, RoutedEventArgs args)
+	{
+		type = "circle";
+	}
+	private void button_click_square(object sender, RoutedEventArgs args)
+	{
+		type = "square";
+	}
+	private void button_click_triangle(object sender, RoutedEventArgs args)
+	{
+		type = "triangle";
+	}
 }
